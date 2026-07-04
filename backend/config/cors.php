7f086b56cb6c722bef23e55d2f8c9efcec17fff3
Mac <mac@Rayans-MacBook-Pro.local>
2026-07-05 00:23:28 +0500

    add vercel domain to cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -9,6 +9,7 @@ return [
     'allowed_origins' => [
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://realestate-rouge-two.vercel.app',
 ],
 
     'allowed_origins_patterns' => [],
